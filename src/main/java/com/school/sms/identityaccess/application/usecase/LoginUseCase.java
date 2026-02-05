828da1f603df75be070c4331067b6349841907d4
rifanmusthafa <rifanmusthafa7@gmail.com>
2026-02-05 17:30:20 +0530

    fix login usecase issue

--- src/main/java/com/school/sms/identityaccess/application/usecase/LoginUseCase.java
+++ src/main/java/com/school/sms/identityaccess/application/usecase/LoginUseCase.java
@@ -1,5 +1,7 @@
 package com.school.sms.identityaccess.application.usecase;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.school.sms.identityaccess.domain.model.User;
@@ -12,6 +14,9 @@ public class LoginUseCase {
 
     private final UserRepository repository;
     private final JwtUtil jwtUtil;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public LoginUseCase(UserRepository repository, JwtUtil jwtUtil) {
         this.repository = repository;
@@ -23,8 +28,9 @@ public class LoginUseCase {
         User user = repository.findByUsername(new Username(username))
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
-        if (!user.getPassword().value().equals(password))
+        if (!passwordEncoder.matches(password, user.getPassword().value())) {
             throw new RuntimeException("Invalid credentials");
+        }
 
         return jwtUtil.generate(user.getUsername().value(), user.getRole());
     }
