bbe314e4edeec4d6df1f728488de05864d384193
rifanmusthafa <rifanmusthafa7@gmail.com>
2026-02-05 17:48:23 +0530

    password encoder bean inside securityfilterchain

--- src/main/java/com/school/sms/identityaccess/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/school/sms/identityaccess/infrastructure/security/SecurityConfig.java
@@ -3,13 +3,12 @@ package com.school.sms.identityaccess.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -56,6 +55,10 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
